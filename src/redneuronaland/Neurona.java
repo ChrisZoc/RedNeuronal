45d61412f28f687d64a1b7891d54c4a2bec1d28f
Francisco <javier_cameros09@hotmail.com>
2016-04-17 15:41:01 -0500

    Clase neruona implementada

---
+++ src/redneuronaland/Neurona.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package redneuronaland;
+
+/**
+ *
+ * @author FRK
+ */
+public class Neurona {
+   private double w1;
+   private double w2;
+   private double umbral;
+   private double alpha;
+   private int target;
+   private double alphaTY;
+   private double incrementoW1;
+   private double incrementoW2;
+   private double incrementoUmbral;
+   
+
+    public Neurona(double w1, double w2, double umbral, double alpha) {
+        this.w1 = w1;
+        this.w2 = w2;
+        this.umbral = umbral;
+        this.alpha = alpha;
+        alphaTY = 0;
+        incrementoW1 = 0;
+        incrementoW2 = 0;
+        incrementoUmbral = 0;
+    }
+    
+    public double [] epoca (int x1, int x2, int t){
+        double [] array = new double[4];
+        w1 += incrementoW1;
+        w2 += incrementoW2;
+        umbral += incrementoUmbral;
+        this.target = t;
+        double a = (x1 * w1) + (x2 * w2);
+        int y;
+        if(a>=umbral)y = 1;
+        else y = 0;
+        if(y != target){
+            this.alphaTY = this.alpha*(target - y);
+            this.incrementoW1 = alphaTY * x1;
+            this.incrementoW1 = alphaTY * x2;
+            this.incrementoUmbral = -1 *alphaTY;
+        }
+        array[0]=alphaTY;
+        array[1]=incrementoW1;
+        array[2]=incrementoW2;
+        array[3]=incrementoUmbral;
+        return array;
+    }
+
+    public double getW1() {
+        return w1;
+    }
+
+    public double getW2() {
+        return w2;
+    }
+
+    public double getUmbral() {
+        return umbral;
+    }
+
+    public double getAlpha() {
+        return alpha;
+    }
+
+    public int getTarget() {
+        return target;
+    }
+
+    public double getAlphaTY() {
+        return alphaTY;
+    }
+
+    public double getIncrementoW1() {
+        return incrementoW1;
+    }
+
+    public double getIncrementoW2() {
+        return incrementoW2;
+    }
+
+    public double getIncrementoUmbral() {
+        return incrementoUmbral;
+    }
+    
+   
+           
+   
+          
+}
--- src/redneuronaland/RedNeuronalAND.java
+++ src/redneuronaland/RedNeuronalAND.java
@@ -5,6 +5,8 @@
  */
 package redneuronaland;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author FRK
@@ -15,7 +17,23 @@ public class RedNeuronalAND {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String aux = JOptionPane.showInputDialog(null,"Ingresa el peso W1 , el W2, el umbral y alpha", JOptionPane.WARNING_MESSAGE);
+        String [] array = aux.split(",");
+        double w1 = Double.parseDouble(array[0]);
+        double w2 = Double.parseDouble(array[1]);
+        double umbral = Double.parseDouble(array[2]);
+        double alpha = Double.parseDouble(array[3]);
+        
+        Neurona neuro = new Neurona(w1,w2,umbral, alpha);
+        aux = JOptionPane.showInputDialog(null,"Ingresa el x1, x2", JOptionPane.WARNING_MESSAGE);
+        array = aux.split(",");
+        double [] arrayDouble;
+        while(true){
+            arrayDouble = neuro.epoca(Integer.parseInt(array[0]),Integer.parseInt(array[1]), 0);
+        }
+
+        
+        
     }
     
 }
